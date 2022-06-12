6e6820afe34cfbe4cf244b419a462b320d855fa8
Peter Milne <peter.milne@d-tacq.com>
2022-06-12 13:52:01 +0100

    --save=1 :: store events in a file per channel.
    example
    [pgm@hoy5 ACQ494_QA]$ ./gpx2_simulate -d 2 -A 11,12,13 --stop0=123 --stop_noise=40 --max_events=5 | ./gpx2_decode -d 1 --save=1
    event_rate 0 Hz
    active channels 11,12,13
    channel:11
    channel:12
    channel:13
         0,11,0.000001845616
         1,12,0.000002684477
         2,13,0.000005872148
         3,11,0.000000267895
         4,12,0.000000603439
         5,13,0.000000100123
         6,11,0.000006743236
         7,12,0.000000200123
         8,13,0.000000367895
         9,11,0.000004158882
        10,12,0.000006172148
        11,13,0.000004494426
        12,11,0.000000735667
        13,12,0.000006775464
        14,13,0.000001238983
    [pgm@hoy5 ACQ494_QA]$ ls -l *.dat
    -rw-rw-r-- 1 pgm pgm 40 Jun 12 13:50 gpx2_11.dat
    -rw-rw-r-- 1 pgm pgm 40 Jun 12 13:50 gpx2_12.dat
    -rw-rw-r-- 1 pgm pgm 40 Jun 12 13:50 gpx2_13.dat
    [pgm@hoy5 ACQ494_QA]$ time ./gpx2_simulate -d 2 -A 11,12,13 --stop0=123 --stop_noise=40 --max_events=1000000 | ./gpx2_decode -d 0 --save=1
    event_rate 0 Hz
    active channels 11,12,13
    channel:11
    channel:12
    channel:13
    
    real	0m1.247s
    user	0m1.929s
    sys	0m0.052s
    [pgm@hoy5 ACQ494_QA]$ ls -l *.dat
    -rw-rw-r-- 1 pgm pgm 8000000 Jun 12 13:51 gpx2_11.dat
    -rw-rw-r-- 1 pgm pgm 8000000 Jun 12 13:51 gpx2_12.dat
    -rw-rw-r-- 1 pgm pgm 8000000 Jun 12 13:51 gpx2_13.dat
    
     On branch pgm1
    	modified:   gpx2_decode.cpp

--- gpx2_decode.cpp
+++ gpx2_decode.cpp
@@ -26,15 +26,40 @@
 
 
 #include <stdio.h>
+#include <assert.h>
 #include "popt.h"
+#include <vector>
+#include <map>
+
+using namespace std;
 
 #include "gpx2_format.h"
 
+struct ChannelData {
+	int sc;
+	FILE *fp;
+
+	ChannelData(int _sc): sc(_sc) {
+		char fname[80];
+		snprintf(fname, 80, "gpx2_%02d.dat", sc);
+		fp = fopen(fname, "w");
+		assert(fp);
+	}
+	ChannelData(): sc(0), fp(0) {}
+};
+
+typedef map<unsigned, ChannelData> CMAP;
+
 namespace G {		/* put any globals in here */
 	int verbose = 0;
+	int save;
+	CMAP cmap;
 };
 
 struct poptOption opt_table[] = {
+	{
+	  "save", 's', POPT_ARG_INT, &G::save, 0, "set to 1 to save data"
+	},
 	{
 	  "verbose", 'd', POPT_ARG_INT, &G::verbose, 0, "set debug level"
 	},
@@ -51,17 +76,33 @@ int decode(void)
 		if (G::verbose > 2){
 			fprintf(stderr, "%d,%016llx\n", event, tmp);
 		}
-		unsigned sc, nref, stop;
-		gpx_from_raw(tmp, sc, nref, stop);
+
+		unsigned sc;
+		double seconds = gpx_from_raw(tmp, sc);
+
 		if (G::verbose == 1){
-			unsigned sc;
-			double seconds = gpx_from_raw(tmp, sc);
+
 			fprintf(stderr, "%6d,%2d,%14.12f\n", event, sc, seconds);
 		}
 		if (G::verbose > 1){
+			unsigned sc, nref, stop;
+			gpx_from_raw(tmp, sc, nref, stop);
 			fprintf(stderr, "%6d,%2d,%8d,%8d\n", event, sc, nref, stop);
 		}
 		event += 1;
+
+		if (G::save){
+			auto cmap = G::cmap.find(sc);
+			FILE* fp;
+			if (cmap != G::cmap.end()){
+				fp = cmap->second.fp;
+			}else{
+				ChannelData* cdata = new ChannelData(sc);
+				G::cmap[sc] = *cdata;
+				fp = cdata->fp;
+			}
+			fwrite(&seconds, sizeof(double), 1, fp);
+		}
 	}
 	return 0;
 }
