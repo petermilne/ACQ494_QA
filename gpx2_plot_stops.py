986455ad2da81a2840585fc063b730d32f8400a1
dt100 <dt100@naboo.d-tacq>
2022-06-16 15:00:14 +0100

    full auto capture, decode, plot  Committer: dt100 <dt100@naboo.d-tacq>  On branch pgm1 	new file:   acq494_validation_test 	modified:   gpx2_plot_stops.py

--- gpx2_plot_stops.py
+++ gpx2_plot_stops.py
@@ -1,29 +1,48 @@
+#!/usr/bin/env python3
 import numpy as np
 import matplotlib.pyplot as plt
+import argparse
 import sys
+import acq400_hapi
+import re
 
-path = sys.argv[1]
+def make_title(serial, ch, stops):
+	title = 'ACQ494FMC Stop Histogram s/n {} ch {}'.format(serial, ch)
+	m = np.mean(stops)
+	s = np.std(stops)
+	p = np.ptp(stops)
+   
+	title += str(round(m, 1)) + ' ± ' + str(round(s, 1)) + ' ps ' + '(' + str(round(p, 1)) + ' ptp)'
+	return title
 
-y = np.fromfile(path, dtype=np.double)
-yn = 1e12* np.mod(y, 100e-9)
-x = list(range(1, len(y) + 1))
 
-plt.subplot(311)
-plt.plot(y)
-plt.ylabel("SECONDS")
-plt.xlabel("Sample Count")
-plt.title(path)
+def plot(times, serial, ch):
+	seconds = np.fromfile(times, dtype=np.double)
+	stops = 1e12* np.mod(seconds, 100e-9)
 
-plt.subplot(312)
-plt.plot(yn)
-plt.ylabel("STOP ps")
-plt.xlabel("Sample")
-plt.title("")
+	plt.title(make_title(serial, ch, stops))
+	plt.hist(stops, bins=100)
+	plt.xlabel("stop values in ps")
+	plt.ylabel("Freq")
+	plt.show()
 
-plt.subplot(313)
-plt.hist(yn, bins=100)
-plt.xlabel("ps")
-plt.ylabel("Frequency")
+def get_args(argStr=None):
+    parser = argparse.ArgumentParser(description='gpx2_plot_stops')
+    parser.add_argument('--times', '-t', default='gpx2_11.dat', help='file containing double times[n] eg gpx2_11.dat')
+    parser.add_argument('uut', nargs=1)
+    return parser.parse_args(argStr)
 
-plt.tight_layout()
-plt.show(block=1)
+def run_main():
+    args = get_args()
+    uut = acq400_hapi.factory(args.uut[0])
+    m = re.search(r'gpx2_([0-9])([0-9]).dat', args.times)
+    print(m.group(0))
+    site = m.group(1)
+    ch = m.group(2)
+	
+    serial = uut.svc['s{}'.format(site)].SERIAL
+    plot(args.times, serial, ch)
+
+
+if __name__ == "__main__":
+	run_main()
