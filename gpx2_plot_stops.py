1fac68fdd3807f9aec6be430a0841fc547d2c538
dt100 <dt100@naboo.d-tacq>
2022-06-16 17:43:50 +0100

    appends csv file  Committer: dt100 <dt100@naboo.d-tacq>  On branch pgm1 	modified:   gpx2_plot_stops.py

--- gpx2_plot_stops.py
+++ gpx2_plot_stops.py
@@ -5,26 +5,25 @@ import argparse
 import sys
 import acq400_hapi
 import re
+import os
 
 def make_title(serial, ch, stops):
-	title = 'ACQ494FMC Stop Histogram s/n {} ch {}'.format(serial, ch)
-	m = np.mean(stops)
-	s = np.std(stops)
-	p = np.ptp(stops)
-   
-	title += '\n' + str(round(m, 1)) + ' ± ' + str(round(s, 1)) + ' ps ' + '(' + str(round(p, 1)) + ' ptp)'
-	return title
+    title = 'ACQ494FMC Stop Histogram s/n {} ch {}'.format(serial, ch)
+    m = np.mean(stops)
+    s = np.std(stops)
+    p = np.ptp(stops)
+
+    title += '\n' + str(round(m, 1)) + ' ± ' + str(round(s, 1)) + ' ps ' + '(' + str(round(p, 1)) + ' ptp)'
+    return title
 
 
-def plot(times, serial, ch):
-	seconds = np.fromfile(times, dtype=np.double)
-	stops = 1e12* np.mod(seconds, 100e-9)
-
-	plt.title(make_title(serial, ch, stops))
-	plt.hist(stops, bins=100)
-	plt.xlabel("stop values in ps")
-	plt.ylabel("Freq")
-	plt.show()
+def plot(stops, title, serial, ch):
+    plt.title(title)
+    plt.hist(stops, bins=100)
+    plt.xlabel("stop values in ps")
+    plt.ylabel("Freq")
+    plt.savefig('{}.{}.png'.format(serial, ch))
+    plt.show()
 
 def get_args(argStr=None):
     parser = argparse.ArgumentParser(description='gpx2_plot_stops')
@@ -39,10 +38,22 @@ def run_main():
     print(m.group(0))
     site = m.group(1)
     ch = m.group(2)
-	
+
     serial = uut.svc['s{}'.format(site)].SERIAL
-    plot(args.times, serial, ch)
+    seconds = np.fromfile(args.times, dtype=np.double)
+    stops = 1e12* np.mod(seconds, 100e-9)
+    plot(stops, make_title(serial, ch, stops), serial, ch)
+
+    summary_file = 'acq494_validation.csv'
+    if not os.path.exists(summary_file):
+        with open(summary_file, 'a') as f:
+            line = [ 'serial','uut', 'site', 'ch', 'mean', 'std', 'ptp' ]
+            f.write(",".join(line) + os.linesep)
+
+    with open(summary_file, 'a') as f:
+        line = [ serial, uut.uut, site, ch, str(np.mean(stops)), str(np.std(stops)), str(np.ptp(stops)) ]
+        f.write(",".join(line) + os.linesep)
 
 
 if __name__ == "__main__":
-	run_main()
+    run_main()
