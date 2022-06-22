1c1ef7853e2d1d53eb149d0696367bcfbf0e0686
Peter Milne <peter.milne@d-tacq.com>
2022-06-22 08:45:07 +0100

    update unit test for format change  On branch pgm1 	modified:   gpx2_format_unit_test.cpp

--- gpx2_format_unit_test.cpp
+++ gpx2_format_unit_test.cpp
@@ -12,6 +12,7 @@
 void mask_test(){
 	fprintf(stderr, "mask_test %016llx shl %d\n", GPX_SITE_MASK, bit(GPX_SITE_MASK));
 	fprintf(stderr, "mask_test %016llx shl %d\n", GPX_CHAN_MASK, bit(GPX_CHAN_MASK));
+	fprintf(stderr, "mask_test %016llx shl %d\n", GPX_FLAGS_MASK, bit(GPX_FLAGS_MASK));
 	fprintf(stderr, "mask_test %016llx shl %d\n", GPX_NREF_MASK, bit(GPX_NREF_MASK));
 	fprintf(stderr, "mask_test %016llx shl %d\n", GPX_STOP_MASK, bit(GPX_STOP_MASK));
 }
