dd88eeedca7af8615fef5142e10a40e9178aef56
peter milne <peter.milne@naboo>
2022-07-01 20:15:21 +0100

    plot everything again..  On branch ZYNQ 	modified:   plot.TAI128.py

--- plot.TAI128.py
+++ plot.TAI128.py
@@ -1,8 +1,24 @@
+#!/usr/bin/env python3
 import numpy as np
 import matplotlib.pyplot as plt
 import sys
+import argparse
+import acq400_hapi
+import re
+import os
 
 path = sys.argv[1]
+
+try:
+   uut = acq400_hapi.factory(sys.argv[2])
+   m = re.search(r'gpx2_([0-9])([0-9]).TAI128', path)
+   site = m.group(1)
+   ch = m.group(2)
+   serial = uut.svc['s{}'.format(site)].SERIAL
+except:
+   serial = "unknown"
+
+
 
 #dt = np.dtype([('time', [('tai', np.uint64), ('nref', np.uint32), ('stop', np.uint32)])])
 dt = np.dtype([('tai', np.uint64), ('nref', np.uint32), ('stop', np.uint32)])
@@ -20,11 +36,11 @@ def make_title(serial, ch, stops):
     m = np.mean(stops)
     s = np.std(stops)*1e6
     p = np.ptp(stops)*1e6
-    title += '\n' + str(round(m, 1)) + ' ± ' + str(round(s, 1)) + ' ps ' + '(' + str(round(p, 1)) + ' ptp)'
+    title += '\n' + str(round(m, 1)) + ' us ' + ' ± ' + str(round(s, 1)) + ' ps ' + '(' + str(round(p, 1)) + ' ptp)'
     return title
 
 dtus = np.multiply(np.subtract(shot_times[1:],shot_times[0:-1]), 1e6)
-plt.title(make_title("123", 1, dtus))
+plt.title(make_title(serial, 1, dtus))
 plt.xlabel("sample")
 plt.ylabel("DT [us]")
 plt.plot(dtus)
